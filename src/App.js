4b4d40514c718a19847c349061f37211cfb45e0d
Abdelmadjid96 <abdelmadjid.rahal07@gmail.com>
2023-01-20 20:00:16 +0100

    <refactore>: refactor app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import React from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    Hello
+  </div>
+);
 
 export default App;
