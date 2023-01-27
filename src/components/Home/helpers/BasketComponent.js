ece53ce8fcaa445ad4ad6a90faf9d2064d9d1a6b
Abdelmadjid96 <abdelmadjid.rahal07@gmail.com>
2023-01-27 21:53:00 +0100

    <feat>: getting info and dispatch function from store

--- src/components/Home/helpers/BasketComponent.js
+++ src/components/Home/helpers/BasketComponent.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import BasketProductComponent from '../../common/BasketProduct';
 import TotalBullComponent from '../../common/TotalBull';
 
-const BasketComponent = () => (
-  <div>
-    <BasketProductComponent />
-    <TotalBullComponent />
-  </div>
-);
+const BasketComponent = () => {
+  const products = useSelector((state) => state.products);
+  return (
+    <div>
+      {!!products.products.length && (
+        products.products.map((product) => (
+          <BasketProductComponent key={product.key} data={product} />
+        ))
+      )}
+      <TotalBullComponent
+        subTotal={products.subTotal}
+        discount={products.discount}
+        total={products.total}
+      />
+    </div>
+  );
+};
 
 export default BasketComponent;
