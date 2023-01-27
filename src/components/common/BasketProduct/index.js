56767ccee5dc97c18c023ddbaee14ec3419b12af
Abdelmadjid96 <abdelmadjid.rahal07@gmail.com>
2023-01-27 21:52:02 +0100

    <feat>: make a BasketProduct a reusable componenet

--- src/components/common/BasketProduct/index.js
+++ src/components/common/BasketProduct/index.js
@@ -1,24 +1,51 @@
+/* eslint-disable react/default-props-match-prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+
 import Counter from '../Counter';
-
 import styles from './basketProduct.module.css';
+import { decrementQuantity, incrementQuantity } from '../../../redux/reducers/productsSlice';
 
-const BasketProductComponent = () => (
-  <div className={styles.productContainer}>
-    <div className={styles.imageWrapper}>
-      <img src="product/butter.jpeg" alt="aaa" />
-    </div>
-    <div className={styles.contentContainer}>
-      <span>Butter</span>
-      <div className={styles.counterContainer}>
-        <span>quantity</span>
-        <Counter minNumber={1} maxNumber={20} />
+const BasketProductComponent = ({ data }) => {
+  const {
+    image, title, quantity, key, allPrice, price,
+  } = data;
+  const dispatch = useDispatch();
+  return (
+    <div className={styles.productContainer}>
+      <div className={styles.imageWrapper}>
+        <img src={image} alt={title} />
+      </div>
+      <div className={styles.contentContainer}>
+        <span>{title}</span>
+        <div className={styles.counterContainer}>
+          <Counter
+            decrementQuantity={() => dispatch(decrementQuantity(key))}
+            incrementQuantity={() => dispatch(incrementQuantity(key))}
+            quantity={quantity}
+          />
+        </div>
+      </div>
+      <div className={styles.price}>
+        <span>{`£${allPrice || price}`}</span>
       </div>
     </div>
-    <div className={styles.price}>
-      <span>£1.60</span>
-    </div>
-  </div>
-);
+  );
+};
+
+BasketProductComponent.propTypes = {
+  data: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+    allPrice: PropTypes.string,
+    quantity: PropTypes.number.isRequired,
+    key: PropTypes.number.isRequired,
+  }).isRequired,
+};
+BasketProductComponent.defaultProps = {
+  allPrice: 0,
+};
 
 export default BasketProductComponent;
